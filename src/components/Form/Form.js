01360a7940e2392f9dbc53b3684313f3013f4330
Vinicius Hiago Martins <viniciushiagomartins@hotmail.com>
2024-08-22 15:16:56 -0300

    feature: permitir campo tipo date no formulário genérico.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,8 @@
 import styles from "./Form.module.css";
 import { Button, TextField } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import React from "react";
 
 export default function GenericForm({
@@ -8,28 +11,54 @@ export default function GenericForm({
   handleSubmit,
   fields,
   submitLabel,
+  handleDateChange,
 }) {
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
-      {fields.map((field) => (
-        <TextField
-          key={field.name}
-          name={field.name}
-          label={field.label}
-          value={formState[field.name]}
-          onChange={handleInputChange}
-          fullWidth
-          multiline={field.multiline}
-          rows={field.rows || 1}
-          InputLabelProps={{
-            style: { color: "#ffffff" },
-          }}
-          InputProps={{
-            className: styles.formField,
-          }}
-          sx={{ marginTop: "10px" }}
-        />
-      ))}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        {fields.map((field) =>
+          field.type === "date" ? (
+            <DatePicker
+              key={field.name}
+              label={field.label}
+              value={formState[field.name]}
+              onChange={(date) => handleDateChange(field.name, date)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  fullWidth
+                  className={`${styles.formField} ${styles.datePickerField}`}
+                  InputLabelProps={{
+                    style: { color: "#ffffff" },
+                  }}
+                  InputProps={{
+                    style: { color: "#ffffff" },
+                  }}
+                  sx={{ marginTop: "10px" }}
+                />
+              )}
+            />
+          ) : (
+            <TextField
+              key={field.name}
+              name={field.name}
+              label={field.label}
+              value={formState[field.name]}
+              onChange={handleInputChange}
+              fullWidth
+              multiline={field.multiline}
+              rows={field.rows || 1}
+              InputLabelProps={{
+                style: { color: "#ffffff" },
+              }}
+              InputProps={{
+                className: styles.formField,
+              }}
+              sx={{ marginTop: "10px" }}
+            />
+          ),
+        )}
+      </LocalizationProvider>
       <Button
         type="submit"
         variant="contained"
