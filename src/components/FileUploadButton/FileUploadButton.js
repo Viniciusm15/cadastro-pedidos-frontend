3b2dad64ed789ceaf8fc3e872e8e4b16e38c7385
Viniciusm15 <viniciushiagomartins00@gmail.com>
2024-11-12 15:50:26 -0300

    feat: criação do componente para controlar importações.

---
+++ src/components/FileUploadButton/FileUploadButton.js
@@ -0,0 +1,38 @@
+import styles from "./FileUploadButton.module.css";
+import React, { useState } from "react";
+
+export default function FileUploadButton({
+  onUpload,
+  accept = "*/*",
+  buttonText = "Choose file",
+}) {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      const validImageTypes = ["image/jpeg", "image/png", "image/gif"];
+      if (validImageTypes.includes(file.type)) {
+        onUpload(file);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.uploadContainer}>
+      <div className={styles.fileInputWrapper}>
+        <input
+          type="file"
+          accept={accept}
+          onChange={handleFileChange}
+          id="file-upload"
+          className={styles.fileInput}
+        />
+        <label htmlFor="file-upload" className={styles.fileLabel}>
+          {selectedFile ? selectedFile.name : buttonText}
+        </label>
+      </div>
+    </div>
+  );
+}
